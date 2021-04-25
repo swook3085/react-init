ddebf8efadf9c6ebe9ca959ac5de09cac16f0c5c
swook3085 <skyies12@protonmail.com>
2021-04-25 13:48:17 +0900

    code spilt

--- src/container/counter/CounterContainer.js
+++ src/container/counter/CounterContainer.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import { onAgeUp, onAgeDown } from "../../redux/actions/Action";
 import axiosInstance from "../../common/axiosModule";
 
-
 class CounterContainer extends React.Component {
     handleIncrement = () => {
         this.props.onAgeUp();
--- src/layout/Content.js
+++ src/layout/Content.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component, lazy, Suspense } from 'react'
 import { Route, Switch } from 'react-router-dom';
-import HomeContainer from '../container/home/HomeContainer';
-import Counter from '../page/count'
+
+const HomeContainer = lazy(() => import('../container/home/HomeContainer'));
+const Counter = lazy(() => import('../page/count'));
 
 export default class Content extends Component {
     render() {
         return (
             <div id="content">
-                <Route exact path="/" component={HomeContainer} />
-                <Switch>
-                    <Route path="/about/:name" component={Counter}/>
-                    <Route path="/about" component={Counter}/>
-                </Switch>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Route exact path="/" component={HomeContainer} />
+                    <Switch>
+                        <Route path="/about/:name" component={Counter}/>
+                        <Route path="/about" component={Counter}/>
+                    </Switch>
+                </Suspense>
             </div>
         )
     }
